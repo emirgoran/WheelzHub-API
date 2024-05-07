6b90bdf757c11b0796f47d15fdb5f2e65f16db53
Emir <emgoran@edu.aau.at>
2024-05-07 18:06:41 +0200

    Create a new Spring Boot app

---
+++ api/src/main/java/com/wheelzhub/demo/WheelzHubApplication.java
@@ -0,0 +1,13 @@
+package com.wheelzhub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelzHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelzHubApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/wheelzhub/demo/WheelzHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheelzhub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelzHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
